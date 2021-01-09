ed9228e92fdf381235308f3442194e0dd8598468
Khwarz <karoumem01@gmail.com>
2021-01-09 09:42:15 +0100

    feat: adding personal modification to the starting page

--- js/greeting.js
+++ js/greeting.js
@@ -3,23 +3,33 @@ const today = new Date();
 const hour = today.getHours();
 
 // Here you can change your name
-const name = 'John Doe';
+const name = "Emmanuel";
+
+// Possibility to add style to the name
+function createGreeting(greeting) {
+  let tag = document.getElementById("greetings");
+  let nameTag = document.createElement("span");
+  nameTag.classList.add("greetings__name");
+  nameTag.innerText = name;
+  tag.innerText = greeting;
+  tag.appendChild(nameTag);
+}
 
 // Here you can change your greetings
-const gree1 = 'Go to Sleep! ';
-const gree2 = 'Good morning! ';
-const gree3 = 'Good afternoon ';
-const gree4 = 'Good evening ';
-const gree5 = 'Good evening ';
-const gree6 = 'Good evening ';
+const gree1 = "Go to Sleep, ";
+const gree2 = "Good morning, ";
+const gree3 = "Good afternoon ";
+const gree4 = "Good evening ";
+const gree5 = "Good evening ";
+const gree6 = "Good evening ";
 
 // Define the hours of the greetings
 if (hour >= 23 && hour < 5) {
-  document.getElementById('greetings').innerText = gree1 + name;
+  createGreeting(gree1);
 } else if (hour >= 6 && hour < 12) {
-  document.getElementById('greetings').innerText = gree2 + name;
+  createGreeting(gree2);
 } else if (hour >= 12 && hour < 17) {
-  document.getElementById('greetings').innerText = gree3 + name;
+  createGreeting(gree3);
 } else {
-  document.getElementById('greetings').innerText = gree4 + name;
+  createGreeting(gree4);
 }
